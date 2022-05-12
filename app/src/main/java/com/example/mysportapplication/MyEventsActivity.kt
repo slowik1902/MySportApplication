90735ef5d9dedc995bc85261ca45d8938916aae7
slowik1902 <patrykslowinski16@gmail.com>
2022-05-12 16:32:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mysportapplication/MyEventsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mysportapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyEventsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_events)
+    }
+}
